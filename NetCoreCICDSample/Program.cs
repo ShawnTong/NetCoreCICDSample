bf167e56800b55ac08e214746c9b085cf2cabddb
tongshuang <shuang.tong@capgemini.com>
2019-08-18 20:22:56 +0800

    Fix Bug.

--- NetCoreCICDSample/Program.cs
+++ NetCoreCICDSample/Program.cs
@@ -20,5 +20,7 @@ namespace NetCoreCICDSample
         public static IWebHostBuilder CreateWebHostBuilder(string[] args) =>
             WebHost.CreateDefaultBuilder(args)
                 .UseStartup<Startup>();
+
+        //Change
     }
 }
